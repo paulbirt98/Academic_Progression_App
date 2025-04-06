de2f8a7258991f9f6702499d61650bd945b41a6a
Paul Birt <paulbirt1998@gmail.com>
2025-04-06 23:17:21 +0100

    Add connection to the database

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mysql = require('mysql2');
 const path = require('path');
+
+//database connection
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'academic_progression_app',
+    port: '3306'
+});
+
+db.getConnection((err) => {
+    if (err) return console.log(err.message);
+    console.log("connected successfully");
+});
+
+module.exports = db; //exports db... allows other parts of the app to import and use the connection pool
 
 //middleware
 app.use(express.static(path.join(__dirname, './assets'))); //serving static files
